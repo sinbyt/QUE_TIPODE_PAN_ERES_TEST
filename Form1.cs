147a52e552a8d99e46ae3fdb86a14ca65f393ac7
sinbyt <adrian.villarrealgrz@uanl.edu.mx>
2022-05-17 17:24:54 -0500

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QUE_TIPODE_PAN_ERES_TEST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
